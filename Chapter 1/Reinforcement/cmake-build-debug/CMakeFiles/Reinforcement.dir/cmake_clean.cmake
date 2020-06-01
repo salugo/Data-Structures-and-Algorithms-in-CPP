71ceea16cc099595ab8d8d41bde005e675464791
salugo <salugo@ucsd.edu>
2020-05-31 22:08:43 -0700

    Added my project

---
+++ Chapter 1/Reinforcement/cmake-build-debug/CMakeFiles/Reinforcement.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reinforcement.dir/main.cpp.obj"
+  "Reinforcement.exe"
+  "Reinforcement.exe.manifest"
+  "Reinforcement.lib"
+  "Reinforcement.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reinforcement.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
